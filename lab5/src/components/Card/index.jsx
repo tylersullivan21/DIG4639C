31778ae6e0533c5fe4894a2118c4fe65b71f6043
Tyler Sullivan <tyler.sullivan2198@knights.ucf.edu>
2019-11-02 21:22:12 -0400

    structural changes

--- lab5/src/components/Card/index.jsx
+++ lab5/src/components/Card/index.jsx
@@ -11,9 +11,9 @@ const Card = props => {
           <div className="card-name-section">
             <h2 className="card-name-content">{period.name}</h2>
           </div>
-          <div className="temperature-section">
-            <span className="temperature">{period.temperature}</span>
-            <span className="temperature-unit">{period.temperatureUnit}</span>
+          <div className="temp-section">
+            <span className="temp">{period.temperature}</span>
+            <span className="temp-unit">{period.temperatureUnit}</span>
           </div>
           <div className="detailedForecast-section">
             <p className="details">{period.detailedForecast}</p>
