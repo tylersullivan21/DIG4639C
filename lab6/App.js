8b0ac07d77ef8ea608aa9cdeb455e92f9391cfe9
Tyler Sullivan <tyler.sullivan2198@knights.ucf.edu>
2019-11-09 22:30:10 -0500

    added rubric in as comments

--- lab6/App.js
+++ lab6/App.js
@@ -20,12 +20,14 @@ backGroundStyles:{
 }
 
 })
-
+/*Student correctly converted the function into a component*/
+/*Using <Text> components, the student described their favorite book, video games, or TV show and summarized why*/
+/*Student styled the <Text> components*/ 
  export default class App extends React.Component {
   render() {
     return (
       <View style={containerStyles.backgroundStyles}>
- 
+
         <Text style={uniqueStyles.textStyles}>My favorite movie is Monsters Inc</Text>
        
         <Text style={uniqueStyles.textStyles}>One of the reasons Monsters Inc is my favorite movie is because growing up when I played baseball my nickname was sully due to my name being James T Sullivan. This sounded very similar to James P Sullivan and led to me having a bond with the character and the movie.</Text>
