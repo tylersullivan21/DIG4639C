33b69deccb9cd5e4de4056d7c42494d2fdee8956
Tyler Sullivan <tyler.sullivan2198@knights.ucf.edu>
2019-11-09 22:06:19 -0500

    added twitter sign in button and facebook sign in button

--- lab6/App.js
+++ lab6/App.js
@@ -1,26 +1,47 @@
 import React from 'react';
+
 import {  Text, View, StyleSheet } from 'react-native';
+import { SocialIcon } from 'react-native-elements'
+import { checkForUpdateAsync } from 'expo/build/Updates/Updates';
 
 const uniqueStyles = StyleSheet.create({
-  text1: {
+  
+  textStyles: {
     backgroundColor: "magenta",
     color: "cyan",
     margin: 50
   }
+
 })
 
- class App extends React.Component {
+const containerStyles = StyleSheet.create({
+backGroundStyles:{
+  backgroundColor: "cyan",
+}
+
+})
+
+ export default class App extends React.Component {
   render() {
     return (
-      <View>
+      <View style={containerStyles.backgroundStyles}>
+ 
+        <Text style={uniqueStyles.textStyles}>My favorite movie is Monsters Inc</Text>
        
-        <Text style={uniqueStyles.text1}>My favorite movie is Monsters Inc</Text>
-       
-        <Text style={uniqueStyles.text1}>One of the reasons Monsters Inc is my favorite movie is because growing up when I played baseball my nickname was sully due to my name being James T Sullivan. This sounded very similar to James P Sullivan and led to me having a bond with the character and the movie.</Text>
+        <Text style={uniqueStyles.textStyles}>One of the reasons Monsters Inc is my favorite movie is because growing up when I played baseball my nickname was sully due to my name being James T Sullivan. This sounded very similar to James P Sullivan and led to me having a bond with the character and the movie.</Text>
    
+<SocialIcon
+  title='Sign In With Facebook'
+  button
+  type='facebook'
+/>
+<SocialIcon
+  title='Sign In With Twitter'
+  button
+  type='twitter'
+/>
+<Text style={uniqueStyles.textStyles}>Played around with added more react native elements by adding sign in button for twitter and facebook</Text>
       </View>
     );
   }
 }
-
-export default App;
---
+++ lab6/components/index.js
@@ -0,0 +1,21 @@
+import Icon from 'react-native-vector-icons/FontAwesome';
+import { Input } from 'react-native-elements';
+
+
+
+
+
+class Form extends React.Component {
+
+  render() {
+    return(
+        <Input
+        placeholder='BASIC INPUT'
+      />
+      
+    );
+  }
+
+}
+
+export default Form;
