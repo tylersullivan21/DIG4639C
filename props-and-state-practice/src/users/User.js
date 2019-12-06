66558894dc264cbfe7111de7a3d3470c6f53b707
Tyler Sullivan <tyler.sullivan2198@knights.ucf.edu>
2019-12-06 18:04:10 +0900

    dynamic functionality

---
+++ props-and-state-practice/src/users/User.js
@@ -0,0 +1,8 @@
+  
+import React from 'react';
+
+const User = (props) => {
+return (<div>Name: {props.children} Age: {props.age}</div>)
+}
+
+export default User;
---
+++ props-and-state-practice/src/users/Users.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import User from './User';
+
+class Users extends Component {
+
+    state = {
+        users: [
+            {name: "John", age:20},
+            {name: "Billy", age:30},
+            {name: "Amanda", age:40},
+
+        ],
+        
+        title: "Users List"
+
+    };
+
+    makeMeYounger = () => {
+        console.log("clicked");
+        this.setState({
+            users: [
+                {name: "John", age:10},
+                {name: "Billy", age:20},
+                {name: "Amanda", age:30},
+    
+            ],
+        })
+    }
+
+    render(){
+        return (<div>
+            <button onClick={this.makeMeYounger}>Make us 10 years younger</button>
+            <br/>
+    <h1>{this.state.title}</h1>
+            <User age={this.state.users[0].age}>{this.state.users[0].name}</User>
+            <User age={this.state.users[1].age}>{this.state.users[1].name}</User>
+            <User age={this.state.users[2].age}>{this.state.users[2].name}</User>
+        </div>)
+      }
+
+}
+
+ 
+
+
+
+export default Users;
